81f1abc40cbb456f986119408a1f360e079aa96d
Camilamunoz2201 <alxcmr18@gmail.com>
2025-08-03 12:58:44 -0500

    Refactor guessing game logic to use a loop for multiple attempts and improve user prompts

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
-let numeroSecreto = 7; 
-let numeroUsuario = prompt('Me indicas un numero por favor');
-console.log(numeroUsuario)
+let numeroSecreto = 7;
+let numereIntentos = 1;
+let numeroUsuario = 0;
 
-if (numeroUsuario == numeroSecreto) {
-    alert('Felicidades, has acertado el numero secreto');
-} else if (numeroUsuario < numeroSecreto) {
-    alert('El numero secreto es mayor que 4');
+/**
+ * - [] estudiar el concepto de bucle
+ * - [] para que serive parceInt y cualtos mas extisten del parce
+ * - [] que otros tipos de bucles existen y como se usan
+ * - [] que es un bucle infinito y como evitarlo
+ */
+
+while (numeroUsuario !== numeroSecreto) {
+    numeroUsuario = parseInt(prompt("Adivina el número secreto (entre 1 y 10):"));
+    //TODO: Validar que el número ingresado sea un número entre 1 y 10
+
+    console.log(numeroUsuario);
+    if (numeroUsuario === numeroSecreto) {
+        alert(`¡Felicidades! Adivinaste el número secreto es ${numeroUsuario}. Total de intentos: ${numereIntentos}`);
+    } else {
+        if (numeroUsuario < numeroSecreto) {
+            alert("El número secreto es mayor. Inténtalo de nuevo.");
+        } else {
+            alert("El número secreto es menor. Inténtalo de nuevo.");
+        }
+
+        numereIntentos = numereIntentos + 1;
+    }
 }
