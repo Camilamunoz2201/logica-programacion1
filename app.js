c19f082c353d3261f27046fb178a487eb3814266
Camilamunoz2201 <alxcmr18@gmail.com>
2025-08-03 19:35:12 -0500

    Refactorizar la lógica del juego para mejorar el manejo de la entrada del usuario

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
-let numeroSecreto = 7; 
-let numeroUsuario = prompt('Me indicas un numero por favor');
-console.log(numeroUsuario)
+let numeroSecreto = Math.floor(Math.random() * 100) + 1; // Genera un numero secreto entre 1 y 100
+let numeroUsuario = 0;
+let intentos = 1; // Contador de intentos
+//let palabraVeces = 'vez';
+let maximoIntentos = 3;
+console.log(numeroSecreto);
 
-if (numeroUsuario == numeroSecreto) {
-    alert('Felicidades, has acertado el numero secreto');
-} else if (numeroUsuario < numeroSecreto) {
-    alert('El numero secreto es mayor que 4');
+while (numeroUsuario != numeroSecreto) {
+    numeroUsuario = parseInt(prompt('Me indicas un numero entre 1 y 100 por favor'));
+    console.log(numeroUsuario)
+     //intentos = intentos + 1;
+     //intentos++;
+    intentos += 1; // Incrementa el contador de intentos
+
+    if (numeroUsuario == numeroSecreto) {
+        alert(`Felicidades, has acertado el numero secreto es: ${numeroUsuario}. Lo intentaste ${intentos} ${intentos === 1 ? 'vez' : 'veces'}.`);
+    } else if (numeroUsuario > numeroSecreto) {
+        alert('El numero secreto es menor');
+        
+    } else{
+        alert('El numero secreto es mayor');
+      // palabraVeces = 'veces'; 
+       if (intentos > 3) {
+            alert('Has superado el numero de intentos, el numero secreto era: ' + numeroSecreto);
+            break;
+        }
+    }
 }
